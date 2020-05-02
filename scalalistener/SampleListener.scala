818592a20017daeb8d0986514557a6e7406c2fdf
Ashutosh <ashutosh0710@gmail.com>
2020-05-02 14:53:02 +0530

    Minor fix

--- scalalistener/SampleListener.scala
+++ scalalistener/SampleListener.scala
@@ -1,3 +1,11 @@
+package sparkmonitor.listener
+
+import org.apache.spark.scheduler._
+import org.apache.spark._
+import org.apache.spark.TaskEndReason
+import org.apache.spark.JobExecutionStatus
+import org.apache.spark.SparkContext
+
 class SampleListener (conf: SparkConf) extends SparkListener {
 
   override def onApplicationStart(appStarted: SparkListenerApplicationStart): Unit = {
